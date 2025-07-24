69596fa2da7efad5e1eef946e10204e69bc75be2
Kevzzkie <kevinbulfanisay1@gmail.com>
2025-07-24 23:07:18 +0300

    Initial commit: Matrix effect, live map, VPN logic

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Share+Tech+Mono&display=swap" rel="stylesheet" />
+        <style>{`body { font-family: 'Share Tech Mono', monospace; }`}</style>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
